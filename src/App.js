3db19ac4a3727a43c26ca60d9d215861b8045d9a
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-10-20 09:34:42 +0100

    Cleaned Up fetch request

--- src/App.js
+++ src/App.js
@@ -88,13 +88,15 @@ export default function App() {
   }
 
   useEffect(
-    function name(params) {
+    function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (res.ok)
@@ -103,10 +105,14 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
           setIsLoading(false);
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -119,6 +125,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
